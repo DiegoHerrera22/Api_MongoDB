7e9fdd82227394f874628f6bdee1642f24cdf856
Diego Ignacio Herrera Venegas <147668937+DiegoHerrera22@users.noreply.github.com>
2025-11-30 02:34:16 -0300

    Creacion de Api
    Creacion de Api con spring boot

---
+++ src/main/java/com/_Semestre/Api_MongoDb/ApiMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com._Semestre.Api_MongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_Semestre/Api_MongoDb/ApiMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com._Semestre.Api_MongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
